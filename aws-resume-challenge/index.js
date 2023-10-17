ed51d25e7c6d2454976976e17b3c63c004f20ec7
qdiddypop <147552733+qdiddypop@users.noreply.github.com>
2023-10-16 21:55:34 -0400

    update

--- aws-resume-challenge/index.js
+++ aws-resume-challenge/index.js
@@ -1,8 +1,18 @@
 const counter = document.querySelector(".website-counter");
+
 async function updateCounter() {
-    let response = await fetch(
-        "https://cqczz25ken7e5oir43bacfvdwy0cgide.lambda-url.us-east-1.on.aws/");
-    let data = await response.json();
-    
+    try {
+        let response = await fetch(
+            "https://cqczz25ken7e5oir43bacfvdwy0cgide.lambda-url.us-east-1.amazonaws.com/");
+        let data = await response.json();
+
+        // Assuming the response from your Lambda function contains a "count" property
+        if (data && data.count) {
+            counter.textContent = data.count;
+        }
+    } catch (error) {
+        console.error("Error updating counter:", error);
+    }
 }
+
 updateCounter();
