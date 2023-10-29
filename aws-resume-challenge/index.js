35e35772d955c212173b3dadc3d49adb4e800613
qdiddypop <147552733+qdiddypop@users.noreply.github.com>
2023-10-29 13:48:20 -0400

    update

--- aws-resume-challenge/index.js
+++ aws-resume-challenge/index.js
@@ -5,7 +5,7 @@ async function updateCounter() {
     counter.textContent = '0';
 
     let response = await fetch(
-        "https://245zmq3phuc5mk2izugnenqmma0vyqes.lambda-url.us-east-1.on.aws/"
+        "https://4mgwvtn3egg7jdwk7j4d6bvnlq0bqdew.lambda-url.us-east-1.on.aws/"
     );
     let data = await response.json();
 
