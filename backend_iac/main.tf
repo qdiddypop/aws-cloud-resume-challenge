b0cc4d141333478dfcc5339c8450710a503c5244
qdiddypop <147552733+qdiddypop@users.noreply.github.com>
2023-10-29 17:38:32 -0400

    update

--- backend_iac/main.tf
+++ backend_iac/main.tf
@@ -2,7 +2,7 @@ resource "aws_lambda_function" "myfunc" {
   filename         = data.archive_file.zip_the_python_code.output_path
   source_code_hash = data.archive_file.zip_the_python_code.output_base64sha256
   function_name    = "myfunc"
-  role             = aws_iam_role.iam_for_lambda.arn
+  role             = arn:aws:iam::arn:aws:iam::326768895355:role/cloudresume_lambda_role
   handler          = "func.lambda_handler"
   runtime          = "python3.8"
 }
