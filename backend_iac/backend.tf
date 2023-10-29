b33830f0163cd5f3d45d9250d6ab18a7999a5f92
qdiddypop <147552733+qdiddypop@users.noreply.github.com>
2023-10-29 16:04:31 -0400

    update

--- backend_iac/backend.tf
+++ backend_iac/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "qhowell-cloud-resume-challenge"
-    key    = "qhowell-cloud-resume-challenge/backend/terraform.tfstate"
-    region = "us-east-1"
+    bucket = "qhowell-cloud-resume-challenge" # Specify the S3 bucket name
+    key    = "backend/terraform.tfstate"      # Path to the state file within the bucket
+    region = "us-east-1"           # AWS region where the S3 bucket is located
   }
 }
