5a6ac4b9a580150fe4a793322d6c7c68e20bac96
qdiddypop <147552733+qdiddypop@users.noreply.github.com>
2023-10-29 15:36:56 -0400

    Delete backend_iac/backend.tf

--- backend_iac/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "qhowell-cloud-resume-challenge"
-    key    = "qhowell-cloud-resume-challenge/backend/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
